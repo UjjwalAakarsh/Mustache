2bc51bdb10318502d8fc64cce6720eb8c2d93ab9
UjjwalAakarsh <72330364+UjjwalAakarsh@users.noreply.github.com>
2021-02-28 11:47:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+nose_x=0;
+nose_y=0;
+function preload(){
+img=loadImage("https://i.postimg.cc/3x3QzSGq/m.png")
+}
+
+function setup(){
+    canvas=createCanvas(500,500)
+    canvas.center()
+    video=createCapture(VIDEO)
+    video.size(500,500)
+    video.hide()
+    posenet=ml5.poseNet(video,modelloaded)
+    posenet.on("pose",gotposes)
+}
+
+function draw(){
+    image(video,0,0,500,500)
+    image(img,nose_x-5,nose_y-10,50,50)
+    
+}
+
+function modelloaded(){
+    console.log("THIS MODEL HAS BEN LOADED") 
+}
+
+function gotposes(results){
+    if (results.length>0) {
+        console.log(results)
+        nose_x=results[0].pose.nose.x
+        nose_y=results[0].pose.nose.y
+    }
+
+    
+}
